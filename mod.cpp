d1804559c9b0d2f1920102b61a8b45fd3c9184f7
Sgt-Parker <white.nathan14@gmail.com>
2015-12-04 17:59:31 -0500

    added mod.cpp and image

---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "22nd ODST Battalion Modpack";
+picture = "odst_emblem.paa";
+actionName = "Website";
+action = "http://www.22ndodst.us/";
+description = "22nd ODST Modpack DEV";
+logo = "odst_emblem.paa";
+logoOver = "odst_emblem.paa";
+tooltip = "22nd ODST";
+tooltipOwned = "22nd ODST Owned";
+overview = "The modpack of the 22nd ODST Battalion";
+author = "22nd ODST Dev Team";
+overviewPicture = "odst_emblem.paa";
