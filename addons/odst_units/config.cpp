498b27101c214723b7a356314e665d8721df4084
Sgt-Parker <white.nathan14@gmail.com>
2015-12-08 20:11:00 -0500

    added units and classes

--- addons/odst_units/config.cpp
+++ addons/odst_units/config.cpp
@@ -1,32 +1,36 @@
-class cfgVehicles
-{
-	#include "CfgVehicles.hpp"
-};
+#include "script_component.hpp"
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
 
-class cfgWeapons
-{
-	#include "CfgWeapons.hpp"
-};
-
-class cfgFactionClasses
-{
-	class 22nd_battalion_odst_unit
-	{
+class cfgFactionClasses {
+	class 22nd_battalion_odst_unit {
 		displayName = "22nd ODST Battalion";
 		author = "kripto202";
 		priority = 1;
 		side = 1;
-		icon="\z\22nd\addons\odst_units\odst_emblem.paa";
+		icon= PATHTOF(odst_emblem.paa);
 		primaryLanguage="EN";
 		backpack_tf_faction_radio_api="OPTRE_Como_pack_2";
 	};
 };
 
-class cfgVehicleClasses
-{
-	class 22nd_Infantry_class
-	{
-		displayName = "Infantry [ODST]";
+class cfgVehicleClasses {
+	class 22nd_Infantry_ODST_B_class {
+		displayName = "ODST [Black]";
+		author = "kripto202";
+		scope=2;
+		scopeArsenal=2;
+		scopeCurator=2;
+	};
+	class 22nd_Infantry_ODST_M_class {
+		displayName = "ODST [MARPAT]";
+		author = "kripto202";
+		scope=2;
+		scopeArsenal=2;
+		scopeCurator=2;
+	};
+	class 22nd_Infantry_ODST_D_class {
+		displayName = "ODST [Desert MARPAT]";
 		author = "kripto202";
 		scope=2;
 		scopeArsenal=2;
@@ -34,30 +38,24 @@ class cfgVehicleClasses
 	};
 };
 
-class cfgPatches
-{
-	class 22nd_odst_units
-	{
-		requiredAddons[]=
-		{
+class cfgPatches {
+	class 22nd_odst_units {
+		requiredAddons[]= {
 			"A3_Characters_F",
 			"OPTRE_UNSC_Units",
 			"OPTRE_Weapons",
 			"OPTRE_Vehicles",
 			"ACE_Main"
 		};
-		weapons[]=
-		{
+		weapons[] = {
 			"22nd_ODST_kripto202_Helmet_Sniper",
 			"22nd_ODST_kripto202_Helmet_Sniper_dp",
 			"22nd_odst_vest_kripto202"
 		};
 
-		units[]=
-		{
-			"OPTRE_UNSC_ODST_Soldier",
-			"22nd_ODST_Soldier_Sniper",
-			"22nd_odst_kripto202"
+		units[] =	{
+			"22nd_odst_mKripto",
+			"22nd_odst_nParker"
 		};
 	};
 };
